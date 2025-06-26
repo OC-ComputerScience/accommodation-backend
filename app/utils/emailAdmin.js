d94526f31304285f93df232dbe90dab8afa68d34
danielndayegamiye <97477024+danielndayegamiye@users.noreply.github.com>
2025-06-26 14:58:16 -0500

    Update emailAdmin.js

--- app/utils/emailAdmin.js
+++ app/utils/emailAdmin.js
@@ -36,7 +36,7 @@ exports.send_non_faculty_emails = async (studentId, semesterId, accomCatIds) => 
 
     // Prepare email details
     const recipient = "d.ndayegamiye@eagles.oc.edu"; // change this email to be dynamic
-    const subject = message.description;
+    const subject = fillTemplate(message.description, { studentName: `${student.fName} ${student.lName}` });
     const messageData = {
       studentName: `${student.fName} ${student.lName}`,
       categoryName: category.name,
